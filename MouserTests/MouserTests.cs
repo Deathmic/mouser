8c8873c0613938b20a03c6c72b272f4086d733fa
Deathmic <deathmic@gmail.com>
2019-08-01 20:07:33 +0200

    Add (empty) unit test project

---
+++ MouserTests/MouserTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MouserTests
+{
+    [TestFixture]
+    public class MouserTests
+    {
+        [Test]
+        public void Test1()
+        {
+        }
+    }
+}
